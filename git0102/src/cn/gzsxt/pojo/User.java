05ec41de159f5f4845886844c8d6556a6fe77d01
laowu <1477694553@qq.com>
2018-06-13 19:25:53 +0800

    第一次在celipse提交代码

---
+++ git0102/src/cn/gzsxt/pojo/User.java
@@ -0,0 +1,48 @@
+package cn.gzsxt.pojo;
+
+public class User {
+
+	private Integer id;
+	
+	private String name;
+	
+	private String password;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public User(Integer id, String name, String password) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.password = password;
+	}
+	
+	
+}
